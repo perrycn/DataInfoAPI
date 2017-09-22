aed451047460b0d7b391272a83735ea0698e4a2d
perrycn <perrycn@charter.net>
2017-09-22 12:30:02 -0500

    DataInfoAPI for Hello World check-in

---
+++ DataInfoAPI/DataInfoAPI.Tests/Controllers/DataControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using System.Web.Http.Results;
+using DataInfoAPI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataInfoAPI.Tests.Controllers
+{
+    [TestClass]
+    public class DataControllerTest
+    {
+        [TestMethod]
+        public void GetReturn()
+        {
+            // Arrange
+            DataController controller = new DataController();
+
+            // Act 
+            IHttpActionResult actionResult = controller.GetData();
+            var contentResult = actionResult as OkNegotiatedContentResult<string>;
+            // Assert
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual("Hello World", contentResult.Content);
+        }
+    }
+}
---
+++ DataInfoAPI/DataInfoAPI/Controllers/DataController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace DataInfoAPI.Controllers
+{
+    public class DataController : ApiController
+    {
+        // GET api/Data
+        public IHttpActionResult GetData()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
